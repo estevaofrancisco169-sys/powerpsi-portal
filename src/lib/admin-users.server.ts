cc1286da3e501482bc5d57ef2c26fa286ff7bb58
estevaofrancisco169-sys <estevaofrancisco169@gmail.com>
2026-08-31 17:04:43 -0300

    Update admin-users.server.ts

--- src/lib/admin-users.server.ts
+++ src/lib/admin-users.server.ts
@@ -31,7 +31,7 @@ export async function exigirAdmin(userId: string) {
     .maybeSingle();
   if (error) {
   console.error("[exigirAdmin] erro real:", error);
-  throw new Error("Não foi possível validar suas permissõs.");
+  throw new Error("Não foi possível validar suas permissões.");
 }
   if (!data) throw new Error("Apenas administradores podem gerenciar usuários.");
 }
